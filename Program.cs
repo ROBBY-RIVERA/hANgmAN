3fb9022c6b27c3650536ccbc686dbabc98b38f86
Cine Student <Cine Student@CINELAB08>
2024-01-31 20:49:01 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hANgmAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hang the Man!");
+            Console.WriteLine("Pick a letter than pick more until you have enough or this guy gets it.");
+            Random rand = new Random();
+            int select = rand.Next();
+            string[] word = new string[] {"Gimmick", "Gregarious", "Garden", "Giddy", "Gargoyle", "Gaudy", "Got"};
+        }
+    }
+}
